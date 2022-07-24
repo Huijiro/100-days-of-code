8244aa34b4242fceb505f8a315252665aa949940
Gabriel Rodrigues <huijirohankei@gmail.com>
2022-07-24 11:20:14 -0300

    Made a really shitty version of Pong

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,14 @@
-import './style.css';
+import Pong from './pong';
 
-import Canvas from './canvas';
-import Vector2D from './vector2D';
-
-const app = document.querySelector<HTMLDivElement>('#app')!;
-
-app.innerHTML = `
-  <h1>Day 2. Input handling.</h1>
-`;
-
-const canvas = new Canvas(app);
-
-const ball = new Vector2D(0, 0);
-
-const direction = new Vector2D(0, 0);
-
-canvas.animate(() => {
-  canvas.drawPixel(ball.x, ball.y, 4);
-
-  window.addEventListener('keydown', (event) => {
-    switch (event.key) {
-      case 'ArrowUp':
-        direction.y = -1;
-        break;
-      case 'ArrowDown':
-        direction.y = 1;
-        break;
-      case 'ArrowLeft':
-        direction.x = -1;
-        break;
-      case 'ArrowRight':
-        direction.x = 1;
-        break;
-    }
-  });
-
-  window.addEventListener('keyup', () => {
-    direction.reset();
-  });
-
-  ball.add(direction);
+const game = new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: [Pong],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+    },
+  },
 });
